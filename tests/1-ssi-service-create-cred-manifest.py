851e14efd78807398cef617222a831273b95caa1
nitro-neal <groessler24@gmail.com>
2022-10-28 16:38:45 -0500

    GitHub actions (#3)
    * updating github actions
    
    * update docker
    
    * add working dir
    
    * update to system-test
    
    * update formatting
    
    * more logging
    
    * running with no cache
    
    * changing env ip
    
    * update github action
    
    * remove ds store
    
    * remove comment

--- tests/1-ssi-service-create-cred-manifest.py
+++ tests/1-ssi-service-create-cred-manifest.py
@@ -8,8 +8,18 @@ assert resp.status_code == 200
 
 # Create a did for the issuer
 print("\nCreate a did for the issuer: ")
-resp = requests.put('http://localhost:8080/v1/dids/key', data = json.dumps({"keyType":"Ed25519"}))
+
+with open("./testdata/ssi-service/did-input.json","r") as file:
+    jsonData = json.load(file)
+
+print("\nPUT request to ssi-service with payload:")
+print(jsonData)
+
+resp = requests.put('http://localhost:8080/v1/dids/key', data = json.dumps(jsonData))
+
+print("\n Recieved Response: ")
 print(resp.json())
+
 assert resp.status_code == 201
 
 issuerDID = resp.json()['did']['id']
@@ -18,13 +28,18 @@ assert "did:" in  issuerDID
 # Create a kyc schema for the manifest
 print("\nCreate a kyc schema for the manifest: ")
 
-with open("./testdata/kyc-schema-input.json","r") as file:
+with open("./testdata/ssi-service//kyc-schema-input.json","r") as file:
     jsonData = json.load(file)
 
-resp = requests.put('http://localhost:8080/v1/schemas', data = json.dumps(jsonData))
-print(resp.json())
-assert resp.status_code == 201
+print("\nPUT request to ssi-service with payload:")
+print(jsonData)
 
+resp = requests.put('http://localhost:8080/v1/schemas', data = json.dumps(jsonData))
+
+print("\n Recieved Response: ")
+print(resp.json())
+
+assert resp.status_code == 201
 
 schemaID = resp.json()['id']
 assert len(schemaID) == 36
@@ -32,13 +47,19 @@ assert len(schemaID) == 36
 # Create a credential manifest
 print("\nCreate a credential manifest: ")
 
-with open("./testdata/manifest-input.json","r") as file:
+with open("./testdata/ssi-service//manifest-input.json","r") as file:
     jsonData = json.load(file)
 
 jsonString = json.dumps(jsonData)
 jsonString = jsonString.replace("<ISSUERID>", issuerDID)
 jsonString = jsonString.replace("<SCHEMAID>", schemaID)
 
+print("\nPUT request to ssi-service with payload:")
+print(jsonData)
+
 resp = requests.put('http://localhost:8080/v1/manifests', data = jsonString)
+
+print("\n Recieved Response: ")
 print(resp.json())
+
 assert resp.status_code == 201
---
+++ tests/4-dwn-relay-submit-cred-app.py
@@ -0,0 +1,20 @@
+import json
+import requests
+import pytest
+
+# TODO: This needs to be a dynamic payload because the static payload references a manifest which does not exist
+# Submit Credential Applicaiton
+print("Submit Credential Applicaiton: ")
+
+with open("./testdata/dwn-relay/collections-write-input.json","r") as file:
+    jsonData = json.load(file)
+
+print("\nPosting to dwn-relay with payload:")
+print(jsonData)
+
+resp = requests.post('http://localhost:9000/', json = jsonData)
+
+print("\n Recieved Response: ")
+print(resp.json())
+
+assert resp.status_code == 200
