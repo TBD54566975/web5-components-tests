3e9274464fd6a9b2d09f8f18c44b45ad435a8c8d
Neal <groessler24@gmail.com>
2022-10-28 13:42:03 -0500

    more logging

--- tests/1-ssi-service-create-cred-manifest.py
+++ tests/1-ssi-service-create-cred-manifest.py
@@ -12,8 +12,14 @@ print("\nCreate a did for the issuer: ")
 with open("./testdata/ssi-service/did-input.json","r") as file:
     jsonData = json.load(file)
 
+print("\nPUT request to ssi-service with payload:")
+print(jsonData)
+
 resp = requests.put('http://localhost:8080/v1/dids/key', data = json.dumps(jsonData))
+
+print("\n Recieved Response: ")
 print(resp.json())
+
 assert resp.status_code == 201
 
 issuerDID = resp.json()['did']['id']
@@ -25,10 +31,15 @@ print("\nCreate a kyc schema for the manifest: ")
 with open("./testdata/ssi-service//kyc-schema-input.json","r") as file:
     jsonData = json.load(file)
 
-resp = requests.put('http://localhost:8080/v1/schemas', data = json.dumps(jsonData))
-print(resp.json())
-assert resp.status_code == 201
+print("\nPUT request to ssi-service with payload:")
+print(jsonData)
 
+resp = requests.put('http://localhost:8080/v1/schemas', data = json.dumps(jsonData))
+
+print("\n Recieved Response: ")
+print(resp.json())
+
+assert resp.status_code == 201
 
 schemaID = resp.json()['id']
 assert len(schemaID) == 36
@@ -43,6 +54,12 @@ jsonString = json.dumps(jsonData)
 jsonString = jsonString.replace("<ISSUERID>", issuerDID)
 jsonString = jsonString.replace("<SCHEMAID>", schemaID)
 
+print("\nPUT request to ssi-service with payload:")
+print(jsonData)
+
 resp = requests.put('http://localhost:8080/v1/manifests', data = jsonString)
+
+print("\n Recieved Response: ")
 print(resp.json())
+
 assert resp.status_code == 201
--- tests/2-dwn-relay-install-protocols.py
+++ tests/2-dwn-relay-install-protocols.py
@@ -8,10 +8,13 @@ print("\nConfigure Protocols for DWN-Relay: ")
 with open("./testdata/dwn-relay/protocols-configure.json","r") as file:
     jsonData = json.load(file)
 
+print("\nPosting to dwn-relay with payload:")
 print(jsonData)
 
 resp = requests.post('http://localhost:9000/', json = jsonData)
 
+print("\n Recieved Response: ")
 print(resp.json())
+
 assert resp.status_code == 200
 assert resp.json()['replies'][0]['status']['detail'] == 'Accepted'
