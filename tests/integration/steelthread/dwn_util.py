2a5861089cf8db426af9700c21c647681427b000
Neal <groessler24@gmail.com>
2022-11-21 13:28:28 -0600

    add pytest framework

---
+++ tests/integration/steelthread/dwn_util.py
@@ -0,0 +1,154 @@
+import json
+import time
+import dag_cbor
+import base64
+
+from cid import make_cid, from_bytes
+
+from nacl.encoding import URLSafeBase64Encoder
+from multiformats import multihash
+from multibase import encode
+
+from jose import jwt
+
+def b64_url_encode(value: str) -> str:
+    encoded = base64.urlsafe_b64encode(str.encode(value))
+    result = encoded.rstrip(b"=")
+    return result.decode()
+
+def b64_url_encode_bytes(value: bytes) -> str:
+    encoded = base64.urlsafe_b64encode(value)
+    result = encoded.rstrip(b"=")
+    return result.decode()
+
+def bytesToString(b):
+    return b.decode("utf-8")
+
+def stringToBytes(s):
+    return bytes(s, 'utf-8')
+
+def create_jwt(json_data, secret):
+    token = jwt.encode(json_data, secret, algorithm='HS256')
+    print(token)
+    return token
+
+def create_collection_write_message(private_key, did_public_key, data):    
+    encoded_data = b64_url_encode(json.dumps(data))
+    
+    encoded_data_bytes = encoded_data.encode('utf-8')
+
+    descriptor = {
+        "target": did_public_key,
+        "recipient": did_public_key,
+        "method": "CollectionsWrite",
+        "protocol": "https://identity.foundation/decentralized-web-node/protocols/credential-issuance",
+        "schema": "https://identity.foundation/credential-manifest/schemas/credential-application",
+        "recordId": "0474fd67-2d7e-4657-9844-3dcb8b9c54f3",
+        "dataCid": generate_cid(data),
+        "dateCreated": round(time.time() * 1000),
+        "dataFormat": "application/json"
+    }
+
+    authorization = sign_as_authorization(descriptor, private_key, did_public_key)    
+    message = {"descriptor": descriptor, "authorization": authorization, "encodedData" : encoded_data}
+
+    return message
+
+def create_collection_query_message(private_key, did_public_key):
+    descriptor = {
+        "target": did_public_key,
+        "method": "CollectionsQuery",
+        "dateCreated": round(time.time() * 1000),
+        "filter": {
+            "schema": "https://somehost.com/CredentialManifest",
+        },
+    }
+
+    authorization = sign_as_authorization(descriptor, private_key, did_public_key)
+    message = {"descriptor": descriptor, "authorization": authorization}
+
+    return message
+
+def sign_as_authorization(descriptor, private_key, did_public_key):
+    descriptor_cid = generate_cid(descriptor)
+
+    auth_payload = {"descriptorCid": str(descriptor_cid)}
+    auth_payload_str = json.dumps(auth_payload)
+
+    # Remove spaces for perfect matching of auth payload to js
+    auth_payload_str = auth_payload_str.replace(" ", "")
+
+    auth_payload_base64_str = b64_url_encode(auth_payload_str)
+
+    signature = sign(auth_payload_base64_str, private_key, did_public_key)
+    return signature
+
+def generate_cid(payload):
+    payload_cbor_encoded = dag_cbor.encode(payload)
+    payload_hash = multihash.digest(payload_cbor_encoded, "sha2-256")
+    cid = make_cid(1, "dag-cbor", payload_hash)
+    return bytesToString(cid.encode("base32"))
+
+# TODO: Implement this for generating a collectionsWrite dag-cbor message
+# /**
+#  * @returns V1 CID of the DAG comprised by chunking data into unixfs dag-pb encoded blocks
+#  */
+# export async function getDagCid(data: Data): Promise<CID> {
+#   const dataBytes = toBytes(data);
+#   const chunk = importer([{ content: dataBytes }], undefined, { onlyHash: true, cidVersion: 1 });
+#   let root;
+
+#   for await (root of chunk);
+
+#   return root.cid;
+# }
+def get_dag_cid(data):
+    return from_bytes(data)
+
+
+def sign(payload_str, private_key, did_public_key):
+    protected_header = {
+        "alg": "EdDSA",
+        "kid": did_public_key + "#" + did_public_key.replace("did:key:", ""),
+    }
+
+    protected_header_string = json.dumps(protected_header)
+    protected_header_base64_url_string = b64_url_encode(protected_header_string)
+
+    signing_input_string = (
+        protected_header_base64_url_string + "." + payload_str
+    )
+
+    signing_input_bytes = stringToBytes(signing_input_string)
+
+    signed_b64 = private_key.sign(signing_input_bytes, encoder=URLSafeBase64Encoder)
+
+    # python leaves == even with url base64 encoders so we remove == here..
+    sig = bytesToString(signed_b64.signature.rstrip(b"="))
+
+    return_object = {
+        "payload": payload_str,
+        "signatures": [
+            {
+                "protected": protected_header_base64_url_string,
+                "signature": sig,
+            }
+        ],
+    }
+
+    return return_object
+
+
+def get_did_key_from_bytes(public_key_bytes):
+    public_bytes_list = list(public_key_bytes)
+
+    prefix = [237, 1]
+    codec = bytearray(prefix)
+    codec.extend(public_bytes_list)
+
+    codec_bytes_list = list(codec)
+    encoded_public_key = encode("base58btc", bytes(codec_bytes_list))
+
+    did_public_key = "did:key:" + str(encoded_public_key.decode())
+
+    return did_public_key
---
+++ tests/integration/test_steelthread.py
@@ -0,0 +1,163 @@
+import unittest
+import json
+import requests
+import pytest
+
+from nacl.signing import SigningKey
+import dwn_util
+import ssi_service_util
+
+# globals
+dwn_private_key = SigningKey.generate()
+dwn_did_public_key = dwn_util.get_did_key_from_bytes(dwn_private_key.verify_key.encode())
+create_did_response = None
+create_schema_response = None
+create_cred_manifest_response = None
+
+
+class SSIServiceTestCreateCredentialManifest(unittest.TestCase):
+    @pytest.mark.run(order=1)
+    def test_create_did(self):
+        # Create a did for the issuer
+        print("\nCreate a did for the issuer: ")
+
+        with open("./fixtures/ssi-service/did-input.json", "r") as file:
+            jsonData = json.load(file)
+
+        resp = requests.put("http://localhost:8080/v1/dids/key", data=json.dumps(jsonData))
+
+        print("\n Recieved Response: ")
+        print(resp.json())
+
+        global create_did_response
+        create_did_response = resp.json()
+
+    @pytest.mark.run(order=2)
+    def test_create_schema(self):
+        # Create a kyc schema for the manifest
+        print("\nCreate a kyc schema for the manifest: ")
+
+        issuer_did = create_did_response["did"]["id"]
+        self.assertIn("did:", issuer_did)
+
+        with open("./fixtures/ssi-service/kyc-schema-input.json", "r") as file:
+            jsonData = json.load(file)
+
+        print("\nPUT request to ssi-service with payload:")
+        print(jsonData)
+
+        resp = requests.put("http://localhost:8080/v1/schemas", data=json.dumps(jsonData))
+
+        print("\n Recieved Response: ")
+        print(resp.json())
+
+        global create_schema_response
+        create_schema_response = resp.json()
+
+        self.assertEqual(resp.status_code, 201)
+
+
+    @pytest.mark.run(order=3)
+    def test_create_credential_manifest(self):
+        # Create a credential manifest
+        print("\nCreate a credential manifest: ")
+
+        with open("./fixtures/ssi-service/manifest-input.json", "r") as file:
+            jsonData = json.load(file)
+
+        issuer_did = create_did_response["did"]["id"]
+        self.assertIn("did:", issuer_did)
+
+        schema_id = create_schema_response["id"]
+        self.assertIsNotNone(schema_id)
+
+        jsonData["issuerDID"] = issuer_did
+        jsonData["outputDescriptors"][0]["id"] = schema_id
+
+        print("\nPUT request to ssi-service with payload:")
+        print(jsonData)
+
+        resp = requests.put("http://localhost:8080/v1/manifests", data=json.dumps(jsonData))
+
+        print("\n Recieved Response: ")
+        print(resp.json())
+
+        global create_cred_manifest_response
+        create_cred_manifest_response = resp.json()
+
+        self.assertEqual(resp.status_code, 201)
+
+class DWNRelayInstallProtocols(unittest.TestCase):
+    @pytest.mark.run(order=4)
+    def test_install_protocols(self):
+        # Configure Protocols for DWN-Relay
+        print("\nConfigure Protocols for DWN-Relay: ")
+
+        with open("./fixtures/dwn-relay/protocols-configure.json", "r") as file:
+            jsonData = json.load(file)
+
+        print("\nPosting to dwn-relay with payload:")
+        print(jsonData)
+
+        resp = requests.post("http://localhost:9000/", json=jsonData)
+
+        print("\n Recieved Response: ")
+        print(resp.json())
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json()["replies"][0]["status"]["detail"], "Accepted")
+
+
+class DWNRelayQueryManifestsDynamic(unittest.TestCase):
+    @pytest.mark.run(order=5)
+    def test_query_manifests(self):
+        # Query for the manifest from DWN-Relay
+        message = dwn_util.create_collection_query_message(dwn_private_key, dwn_did_public_key)
+        messages = {"messages": [message]}
+
+        print("\nPosting to dwn-relay with payload:")
+        print(messages)
+
+        resp = requests.post("http://localhost:9000/", json=messages)
+
+        print("\n Recieved Response: ")
+        print(resp.json())
+
+        self.assertEqual(resp.status_code, 200)
+
+class DWNRelaySubmitCredApplicationDynamic(unittest.TestCase):
+    @pytest.mark.run(order=6)
+    def test_submit_cred_app(self):
+
+        cred_manifest_id = create_cred_manifest_response["credential_manifest"]["id"]
+        presentation_definition_id = create_cred_manifest_response["credential_manifest"]["presentation_definition"]["id"]
+
+        with open("./fixtures/dwn-relay/collections-write-cred-app-input.json", "r") as file:
+            cred_app_template = json.load(file)
+
+        cred_app_template["credential_application"]["manifest_id"] = cred_manifest_id
+        cred_app_template["credential_application"]["presentation_submission"]["definition_id"] = presentation_definition_id
+
+        vc = ssi_service_util.create_verifiable_credential()
+        cred_app_template["verifiableCredentials"] = [vc["credentialJwt"]]
+
+        cred_app_jwt = dwn_util.create_jwt(cred_app_template, dwn_private_key.encode())
+
+        cred_app_jwt_object = { "applicationJwt": cred_app_jwt }
+
+        message = dwn_util.create_collection_write_message(dwn_private_key, dwn_did_public_key, cred_app_jwt_object)
+        messages = {"messages": [message]}
+
+        print("\nPosting to dwn-relay with payload:")
+        print(messages)
+
+        resp = requests.post("http://localhost:9000/", json=messages)
+
+        print("\n Recieved Response: ")
+        print(resp.json())
+
+        # TODO: Fix the dynamic collections write message to have the correct DAG CID
+        self.assertEqual(resp.status_code, 400)
+
+if __name__ == '__main__':
+    unittest.main()
