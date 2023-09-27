d2ce4fafdb5abc76ae9901c8dd6f7fbf84dd1e5b
finn <fherzfeld@tbd.email>
2023-09-27 10:52:37 -0700

    single component self test

--- test-suite/main_test.go
+++ test-suite/main_test.go
@@ -10,9 +10,8 @@ import (
 
 func TestMain(m *testing.M) {
 	var serverAup bool
-	var serverBup bool
 
-	for !serverAup || !serverBup {
+	for !serverAup {
 		resp, err := http.Get("http://test-server-a:8080/ready")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "waiting for test-server-a to be ready: %v", err)
@@ -23,15 +22,15 @@ func TestMain(m *testing.M) {
 			}
 		}
 
-		resp, err = http.Get("http://test-server-b:8080/ready")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "waiting for test-server-b to be ready: %v", err)
-			continue
-		} else {
-			if resp.StatusCode == http.StatusOK {
-				serverBup = true
-			}
-		}
+		// resp, err = http.Get("http://test-server-b:8080/ready")
+		// if err != nil {
+		// 	fmt.Fprintf(os.Stderr, "waiting for test-server-b to be ready: %v", err)
+		// 	continue
+		// } else {
+		// 	if resp.StatusCode == http.StatusOK {
+		// 		serverBup = true
+		// 	}
+		// }
 
 		time.Sleep(time.Second)
 		fmt.Println("waiting for both servers to start")
@@ -39,7 +38,7 @@ func TestMain(m *testing.M) {
 
 	status := m.Run()
 
-	for _, server := range []string{"test-server-a", "test-server-b"} {
+	for _, server := range []string{"test-server-a"} {
 		if _, err := http.Get(fmt.Sprintf("http://%s:8080/shutdown", server)); err != nil {
 			fmt.Fprintf(os.Stderr, "error shutting down %s: %v", server, err)
 			os.Exit(1)
