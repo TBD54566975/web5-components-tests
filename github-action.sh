71edfc48e335cd9506af6fb7ad783e16ae4391be
finn <fherzfeld@tbd.email>
2023-09-28 12:49:32 -0700

    exit code from test-client container

--- github-action.sh
+++ github-action.sh
@@ -2,4 +2,4 @@
 set -exuo pipefail
 docker build -t web5-component:latest -f .web5-component/test.Dockerfile .
 cd ${GITHUB_ACTION_PATH}
-docker-compose up --timeout 10
+docker-compose up --timeout 10 --exit-code-from test-client
