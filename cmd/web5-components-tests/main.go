64e0ea897c1549f39c7fcb2aa3d8c02a880f6aac
Finn <139806439+finn-tbd@users.noreply.github.com>
2023-10-05 12:31:43 -0700

    shift focus of the repo to be a common set of tests that can be run against all web5 SDKs (#20)
    this includes being usable as a github action as well as being runnable locally. Documentation
    on how to use this as a web5 component to follow.

---
+++ cmd/web5-components-tests/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"os/exec"
+
+	"github.com/TBD54566975/web5-components-tests/tests"
+	"golang.org/x/exp/slog"
+)
+
+var (
+	testOnly = flag.Bool("test-only", false, "when set, the server is not built and is expected to be already running")
+	server   = flag.String("server", "http://localhost:8080", "url of the server to connect to")
+)
+
+func main() {
+	flag.Parse()
+
+	logger := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})
+	slog.SetDefault(slog.New(logger))
+
+	if !*testOnly {
+		cmd := exec.Command("docker", "build", "-t", "web5-component:latest", "-f", ".web5-component/test.Dockerfile", ".")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			slog.Error("error building server", "error", err)
+			os.Exit(1)
+		}
+
+		cmd = exec.Command("docker", "run", "-p", "8080:8080", "--rm", "web5-component:latest")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Start(); err != nil {
+			slog.Error("error running server", "error", err)
+			os.Exit(1)
+		}
+
+		defer func() {
+			slog.Debug("shutting down server")
+			if err := cmd.Process.Signal(os.Kill); err != nil {
+				panic(err)
+			}
+			if err := cmd.Wait(); err != nil {
+				panic(err)
+			}
+		}()
+	}
+
+	tests.RunTests(*server)
+}
---
+++ tests/credential_issuance.go
@@ -0,0 +1,71 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// {
+// 	"@context": [
+// 	  "https://www.w3.org/2018/credentials/v1",
+// 	],
+// 	"id": "{{ID}}",
+// 	"type": [
+// 	  "VerifiableCredential"
+// 	],
+// 	"issuer": "{{ISSUER}}",
+// 	"issuanceDate": "{{DATE}}",
+// 	"credentialSubject": {
+// 	  "id": "{{SUBJECT}}",
+// 	  "firstName": "bob"
+// 	}
+
+// todo: better struct names all around
+type CredentialIssuanceRequest struct {
+	Credential CredentialIssuanceRequestCredential `json:"credential"`
+	Options    CredentialIssuanceRequestOptions    `json:"options"`
+}
+
+type CredentialIssuanceRequestCredential struct {
+	Context           []string       `json:"@context"`
+	ID                string         `json:"id"`
+	Type              []string       `json:"type"`
+	Issuer            string         `json:"issuer"`
+	IssuanceDate      string         `json:"issuanceDate"`
+	CredentialSubject map[string]any `json:"credentialSubject"`
+}
+
+type CredentialIssuanceRequestOptions struct {
+}
+
+func CredentialIssuanceTest(serverURL string) error {
+	req, err := json.Marshal(CredentialIssuanceRequest{
+		Credential: CredentialIssuanceRequestCredential{
+			Context:      []string{"https://www.w3.org/2018/credentials/v1"},
+			ID:           "???",
+			Type:         []string{"VerifiableCredential"},
+			Issuer:       "????",
+			IssuanceDate: "???",
+			CredentialSubject: map[string]any{
+				"id":        "????",
+				"firstName": "bob",
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post(serverURL+"/credentials/issue", "application/json", bytes.NewReader(req))
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("incorrect status from /credential/issue: %s", resp.Status)
+	}
+
+	return nil
+}
